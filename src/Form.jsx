65188790c315cbed715046dbc89f3382615c9b4b
Adedolapo Adeagbo <dollypalton@Adedolapos-MacBook-Pro.local>
2023-08-04 00:00:43 +0100

    simple color generator

---
+++ src/Form.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const Form = ({ addColor }) => {
+  const [color, setColor] = useState(" ");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addColor(color);
+  };
+
+  return (
+    <section className="container">
+      <h4>Color Generator</h4>
+
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          
+          type="text"
+          value={color}
+          placeholder='#f15025'
+          onChange={(e) => setColor(e.target.value)}
+        />
+
+        <button className="btn" type="submit" style={{ background: color }}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
